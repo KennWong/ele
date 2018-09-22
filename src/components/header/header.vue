3d8306605b3295ab4164aeeb0f41dbe878deb5db
王凯 <wangkai3@asiainfo.com>
2018-09-22 11:38:57 +0800

    创建仓库

---
+++ src/components/header/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="content-wrapper">
+    <div class="avatar">
+      <img width="64" height="64" :src="seller.avatar">
+    </div>
+    <div class="content">
+      <div class="title">
+        <span class="brand"></span>
+        <span class="name">{{seller.name}}</span>
+      </div>
+      <div class="discription">
+
+      </div>
+    </div>
+  </div>
+  <div class="bulletin-wrapper"></div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
