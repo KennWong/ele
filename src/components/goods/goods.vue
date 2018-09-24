290ef1982f042330a961f59f349439367302a54e
KEVIN <kennwong3914@gmail.com>
2018-09-24 10:46:24 +0800

    公告页面

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,59 @@
 <template>
-  <div>goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script>
+  const ERR_OK = 0
+
   export default {
-    name: 'goods'
+    name: 'goods',
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: []
+      }
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.goods = response.data
+        }
+      })
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #e5f0f7
+    .foods-wrapper
+      flex 1
+      background #f7c2bd
 </style>
